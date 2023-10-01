376777faf985ccc68f02c171c8632d2717834f4d
Sheep-IK-LT <yik418@naver.com>
2023-10-01 22:36:11 +0900

    마우스 좌표 찾는 코드 구현

--- Drill06.py
+++ Drill06.py
@@ -10,38 +10,33 @@ mouse = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    global mouseX
+    global mouseY
+    mouseX = TUK_WIDTH // 2
+    mouseY = TUK_HEIGHT //2
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-
-
-
-def rand_mouse():
-    global mouseX, mouseY
-    mouseX = random.randrange(1, 800)
-    mouseY = random.randrange(1, 800)
-    mouse.clip_draw(0, 0, 50, 52, mouseX, mouseY)
-
-
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        elif event.type == SDL_KEYDOWN:
+            mouse.clip_draw(0, 0, 50, 52, x, y)
 
 
 running = True
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT //2
-
 hide_cursor()
 
 
 
 while running:
-    rand_mouse()
     copyX = x
     copyY = y
     while mouseX+5 < x or mouseX-5 > x or mouseY+5 < y or mouseY-5 > y:
         tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-        mouse.clip_draw(0, 0, 50, 52, mouseX, mouseY)
+        handle_events()
         if mouseX-copyX >= 0:
             character.clip_draw(frame * 100, 100, 100, 100, x, y)
         elif mouseX-copyY < 0:

dd3db93efbb26f19e9844a4ecf1b8b112b237894
Sheep-IK-LT <yik418@naver.com>
2023-10-01 23:18:44 +0900

    90퍼센트 구현, 오류 찾기중

--- Drill06.py
+++ Drill06.py
@@ -1,4 +1,3 @@
-import random
 from pico2d import *
 
 
@@ -8,46 +7,47 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 mouse = load_image('hand_arrow.png')
 
+
 def handle_events():
     global running
-    global mouseX
-    global mouseY
-    mouseX = TUK_WIDTH // 2
-    mouseY = TUK_HEIGHT //2
+    global mouseX, mouseY, goalX, goalY
+    global ck
+    ck = False
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN:
-            mouse.clip_draw(0, 0, 50, 52, x, y)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            goalX, goalY = event.x, TUK_HEIGHT - 1 - event.y
+            ck = True
 
 
 running = True
 frame = 0
-x, y = TUK_WIDTH // 2, TUK_HEIGHT //2
-hide_cursor()
-
+x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+mouseX, mouseY = x, y
+ck = False
+goalX, goalY = x,y
 
 
 while running:
-    copyX = x
-    copyY = y
-    while mouseX+5 < x or mouseX-5 > x or mouseY+5 < y or mouseY-5 > y:
-        tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-        handle_events()
-        if mouseX-copyX >= 0:
+    tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    handle_events()
+    if ck:
+        mouse.clip_draw(0, 0, 50, 52, goalX, goalY)
+
+    if ck and (goalX + 5 < x or goalX - 5 > x or goalY + 5 < y or goalY - 5 > y):
+        if goalX - x >= 0:
             character.clip_draw(frame * 100, 100, 100, 100, x, y)
-        elif mouseX-copyY < 0:
+        elif goalX - x < 0:
             character.clip_composite_draw(frame * 100, 100, 100, 100, 0, 'h', x, y, 100, 100)
-        x += (mouseX-copyX) / 60.0
-        y += (mouseY-copyY) / 60.0
-        update_canvas()
-        handle_events()
-        frame = (frame + 1) % 8
-        delay(0.05)
-        clear_canvas()
+        x += (goalX - x) / 60.0
+        y += (goalX - y) / 60.0
+
+
+    update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
 
 close_canvas()
 
